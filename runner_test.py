944e3c43d45dbcf61a275f731f68187ad1173b46
Thomas Bonnevialle <94001440+taumha@users.noreply.github.com>
2023-09-15 15:13:45 +0200

    Update runner_test.py

--- runner_test.py
+++ runner_test.py
@@ -1,13 +1,13 @@
 
 import os 
-from utils.htm_to_json.traducter2 import traducter2
+from utils.htm_to_json.traducter2 import Traducter
 
 if __name__ == "__main__":
     for subdir, dirs, files in os.walk('C:/Users/Imène BETOUCHE/Downloads/bordereaux_csv'):
         if 'toprint.htm' in files:
             # Charger le contenu HTML dans BeautifulSoup
             with open(os.path.join(subdir,"toprint.htm"), "r", encoding="iso-8859-1") as file:
-                result = traducter2(file=file)
+                result = Traducter(file=file)
                 print(result)
                 
     pass
