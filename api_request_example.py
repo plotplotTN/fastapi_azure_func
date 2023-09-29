b27b6b3d548ccbd3a6f2729c2c866f38919cd054
Romain Schaller <131660159+plotplotTN@users.noreply.github.com>
2023-09-29 10:28:42 +0200

    ouioui

--- api_request_example.py
+++ api_request_example.py
@@ -1,15 +1,29 @@
-import requests
 
-# URL of the API endpoint
-url = 'https://fastapitraducter.azurewebsites.net/htm_to_json/'
 
-# Open the .htm file in binary mode
-with open('C:\\Users\\romai\\Downloads\\bordereauxcsv-20230911T144458Z-001\\bordereauxcsv\\Bmmnpve1_fichiers\\toprint.htm', 'rb') as file:
-    # Define the files dictionary with the file data
-    files = {'file': ('filename.htm', file)}
-    
-    # Make the POST request
-    response = requests.post(url, files=files)
+if __name__ == "__main__":
+    from azure.identity import InteractiveBrowserCredential
+    from azure.storage.blob import BlobServiceClient
+    from azure.identity import DefaultAzureCredential
+    from azure.storage.blob import BlobServiceClient
 
-# Print the response from the server
-print(response.text)
+    # Set the Blob Service URL
+    BLOB_SERVICE_URL = "https://aaasnowrequeteur.blob.core.windows.net"
+
+
+    try:
+        #credential = DefaultAzureCredential()
+
+        credential = InteractiveBrowserCredential(additionally_allowed_tenants=["d3f969e5-42a3-4d6c-a617-7b969dd92ea1"])
+        #credential = InteractiveBrowserCredential()
+
+        blob_service_client = BlobServiceClient(account_url=BLOB_SERVICE_URL, credential=credential)
+        container_name = "snow-requeteur"
+        container_client = blob_service_client.get_container_client(container_name)
+        blob_list = container_client.list_blobs()
+        l = [a.name for a in blob_list]
+    except Exception as e:
+        l = str(e)
+
+    # The current credential is not configured to acquire tokens for tenant d3f969e5-42a3-4d6c-a617-7b969dd92ea1.
+
+    print({"blob":l})
